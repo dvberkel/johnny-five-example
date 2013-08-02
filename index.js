8c872f6e94343a01e0e6052f77a693dbb4f57e8f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-02 15:01:52 +0200

    introduce a traffic light abstraction

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var five = require('johnny-five');
+var TrafficLight = require('./lib/traffic-light');
 
 var board = new five.Board();
 
@@ -6,21 +7,6 @@ var redPin = 8;
 var yellowPin = 9;
 var greenPin = 10;
 
-var TrafficLight = function TrafficLight(redPin, yellowPin, greenPin) {
-    this.red = new five.Led(redPin);
-    this.yellow = new five.Led(yellowPin);
-    this.green = new five.Led(greenPin);
-}
-TrafficLight.prototype.stop = function stopSignal() {
-    this.red.on();
-    this.yellow.off();
-    this.green.off();
-}
-TrafficLight.prototype.go = function goSignal() {
-    this.red.off();
-    this.yellow.off();
-    this.green.on();
-}
 
 board.on('ready', function(){
     var led = new five.Led(13);
