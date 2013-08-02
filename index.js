708b2dfde9370e1eba27897e573abcc7a20b82a1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-02 14:43:03 +0200

    create traffic light leds

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ var five = require('johnny-five');
 
 var board = new five.Board();
 
+var redPin = 8;
+var yellowPin = 9;
+var greenPin = 10;
+
 board.on('ready', function(){
     var led = new five.Led(13);
     led.strobe();
 
+    var red = new five.Led(redPin);
+    var yellow = new five.Led(yellowPin);
+    var green = new five.Led(greenPin);
+
+    red.on();
+    yellow.off();
+    green.off();
+
     this.repl.inject({
-	led : led
+	led : led,
+	red : red,
+	yellow : yellow,
+	green : green
     });
 });
